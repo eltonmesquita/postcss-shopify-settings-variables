b84aef7971bf7b065fba98ca0537d7a1b9541dc9
Elton Mesquita <callmeelton@gmail.com>
2016-06-23 15:43:30 -0300

    Fixed lint problems

--- index.js
+++ index.js
@@ -5,28 +5,28 @@ var protocolRegex = /(https?:)?\/\/|data:/g;
 
 module.exports = postcss.plugin('postcss-shopify-settings-variables',
   function (opts) {
-    opts = opts || {};
+      opts = opts || {};
 
     // Work with options here
 
-    return function (css) {
+      return function (css) {
 
-        // Transform CSS AST here
-        css.walk(function (node) {
-            if ( node.type === 'decl' ) {
-                if ( node.value.indexOf('$(') >= 0 ) {
-                    node.value = node.value
-                        .replace(/^([^\$]*)(\$\()([^\)]+)(\))(.*)$/,
-                            function(match, $1, $2, $3, $4, $5) {
-                                return $1 + '{{' + $3 + '}}' + $5;
-                            });
-                }
-                if ( bgUrlRegex.test(node.value) &&
-                    !protocolRegex.test(node.value) ) {
-                    node.value = node.value.replace(bgUrlRegex,
-                        'url({{ "$1" | asset_url }})');
-                }
-            }
-        });
-    };
-});
+          // Transform CSS AST here
+          css.walk(function (node) {
+              if ( node.type === 'decl' ) {
+                  if ( node.value.indexOf('$(') >= 0 ) {
+                      node.value = node.value
+                          .replace(/^([^\$]*)(\$\()([^\)]+)(\))(.*)$/,
+                              function(match, $1, $2, $3, $4, $5) {
+                                  return $1 + '{{' + $3 + '}}' + $5;
+                              });
+                  }
+                  if ( bgUrlRegex.test(node.value) &&
+                      !protocolRegex.test(node.value) ) {
+                      node.value = node.value.replace(bgUrlRegex,
+                          'url({{ "$1" | asset_url }})');
+                  }
+              }
+          });
+      };
+  });
