df73bd6c98e8b8f10f3c808861c81b7c03883966
Elton Mesquita <callmeelton@gmail.com>
2016-06-23 15:21:16 -0300

    Rewritten tests

--- test/test.js
+++ test/test.js
@@ -18,56 +18,56 @@ describe('postcss-shopify-settings-variables', function () {
     /* Write tests here */
 
     it('replace single variable in value', function (done) {
-        test('a{ color: $(headline_color); }',
-            'a{ color: {{ settings.headline_color }}; }', { }, done);
+        test('a{ color: $(settings.headline_color); }',
+            'a{ color: {{settings.headline_color}}; }', { }, done);
     });
 
     it('replace multiple variables in multiple values', function (done) {
-        test('a{ color: $(headline_color); ' +
-                'background-color: $(healine_bg_color); }',
-             'a{ color: {{ settings.headline_color }}; ' +
-                'background-color: {{ settings.healine_bg_color }}; }',
+        test('a{ color: $(settings.headline_color); ' +
+                'background-color: $(settings.healine_bg_color); }',
+             'a{ color: {{settings.headline_color}}; ' +
+                'background-color: {{settings.healine_bg_color}}; }',
                 { }, done);
     });
 
     it('replace single variable in value with pixel unit', function (done) {
-        test('body{ font-size: $(headline_size)px; }',
-             'body{ font-size: {{ settings.headline_size }}px; }', { }, done);
+        test('body{ font-size: $(settings.headline_size)px; }',
+             'body{ font-size: {{settings.headline_size}}px; }', { }, done);
     });
 
     it('replace single variable in value with liquid filter', function (done) {
-        test('a{ font-size: $(headline_size | divided_by: 2)px; }',
-             'a{ font-size: {{ settings.headline_size | divided_by: 2 }}px; }',
+        test('a{ font-size: $(settings.headline_size | divided_by: 2)px; }',
+             'a{ font-size: {{settings.headline_size | divided_by: 2}}px; }',
                 { }, done);
     });
 
     it('replace single variable in value with liquid string filter',
         function (done) {
-            test('a{ font-family: $(font_family | replace: \'+\', \' \'); }',
-                'a{ font-family: {{ settings.font_family ' +
-                    '| replace: \'+\', \' \' }}; }',
+            test('a{ font-family: $(settings.font_family | replace: \'+\', \' \'); }',
+                'a{ font-family: {{settings.font_family ' +
+                    '| replace: \'+\', \' \'}}; }',
                 { }, done);
         });
 
     it('replace single variable in value which has multiple variables',
         function (done) {
-            test('a{ border-bottom: 1px dotted $(border_color); }',
-                'a{ border-bottom: 1px dotted {{ settings.border_color }}; }',
+            test('a{ border-bottom: 1px dotted $(settings.border_color); }',
+                'a{ border-bottom: 1px dotted {{settings.border_color}}; }',
                 { }, done);
         });
 
     it('replace single variable in value when there is quotes',
         function (done) {
-            test('a{ font-family: "$(headline_google_webfont_font)"; }',
+            test('a{ font-family: "$(settings.headline_google_webfont_font)"; }',
                 'a{ font-family: ' +
-                    '"{{ settings.headline_google_webfont_font }}"; }',
+                    '"{{settings.headline_google_webfont_font}}"; }',
                 { }, done);
         });
 
     it('replace single variable in value when there is parenthesis',
         function (done) {
-            test('a{ background: rgba($(header_bg_color), 0.9); }',
-                'a{ background: rgba({{ settings.header_bg_color }}, 0.9); }',
+            test('a{ background: rgba($(settings.header_bg_color), 0.9); }',
+                'a{ background: rgba({{settings.header_bg_color}}, 0.9); }',
                 { }, done);
         });
 
@@ -135,9 +135,9 @@ describe('postcss-shopify-settings-variables', function () {
 
     it('replace variable and url together',
         function (done) {
-            test('a{ background: $(modal_background_color) ' +
+            test('a{ background: $(settings.modal_background_color) ' +
                     'url("newsletter_bg.png"); }',
-                'a{ background: {{ settings.modal_background_color }} ' +
+                'a{ background: {{settings.modal_background_color}} ' +
                     'url({{ "newsletter_bg.png" | asset_url }}); }',
                 { }, done);
         });
